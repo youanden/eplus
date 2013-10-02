51a3cf09f0a118063ccb6f25af77b394d9368210
Georgiy Slobodenyuk <youanden@gmail.com>
2013-10-01 23:25:31 -0500

    Set up initial e-plus directory and configuration structure with some relevant gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EPlus::Application.config.secret_key_base = '25539a91783509de943037534009af2f96dd839ad614884899690cc7055389927f469e794ea29cc5d53d4420d879e7f453fea5553007e2f65a1e32694ddd4c58'
