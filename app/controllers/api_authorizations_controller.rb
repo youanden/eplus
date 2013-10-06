f65ce9b05712da0dd73632cde82a7a8517c99230
Georgiy Slobodenyuk <youanden@gmail.com>
2013-10-05 21:35:18 -0500

    Fix classroom import/redirect bug. Update Model views for visual typographic heirarchy.

--- app/controllers/api_authorizations_controller.rb
+++ app/controllers/api_authorizations_controller.rb
@@ -19,10 +19,11 @@ class ApiAuthorizationsController < ApplicationController
           quizlet_id: quizlet_classroom['id'],
           admin_id: current_admin.id
         }
-        Classroom.find_or_create_by(model_data)
-        redirect_to classrooms_path, notice: t(:imported_classrooms)
-        return
+        classroom = Classroom.find_or_create_by(model_data)
+        classroom.update_attributes model_data
       end
+
+      return redirect_to classrooms_path, notice: t(:imported_classrooms)
     else if api == 'quizlet' and type == 'students'
       path = "users/#{current_admin.username}/groups"
       quizlet_classrooms = QuizletApiHelper.get_response(path, current_admin)
@@ -35,8 +36,7 @@ class ApiAuthorizationsController < ApplicationController
           student_ar.classrooms << classroom unless student_ar.classrooms.include?(classroom)
         end
       end
-      redirect_to students_path, notice: t(:imported_students)
-      return
+      return redirect_to students_path, notice: t(:imported_students)
     end
 
       redirect_to root_path, notice: t(:undefined_import)
